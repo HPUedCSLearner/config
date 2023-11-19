a9d3d4f4f374f4e7d93087a8607f311d45d2c5d7
feng-X99 <feng-X99M-D3>
2023-11-19 21:30:49 +0800

    支持配置命令，切换命令；添加一些注释

--- autoConfigNewMachin/autoConfig.py
+++ autoConfigNewMachin/autoConfig.py
@@ -1,5 +1,7 @@
 import json
 import os
+import sys
+import re
 
 from lib.utils import printBlock
 from lib.utils import loadJson
@@ -17,9 +19,16 @@ def readConfig():
 def runConfigSteps(step, config, switch):
     printBlock(step + ".......")
     if switch[step] == "ON":
-        for command in config[step]:
-            print(config[step][command])
-            os.system(config[step][command])
+        for command_key in config[step]:
+            command_val = config[step][command_key]
+            print(command_val)
+            if re.match(r'cd', command_val, re.M | re.I):
+                command_val = re.sub(r'cd ', "", command_val) # 通过字符串匹配替换 完成匹配删除的效果
+                print(command_val)
+                # 可以添加异常捕获，当文件夹不存在的时候，继续往下跑
+                os.chdir(command_val) # 或者不加，当切换目录失败，以为一下的命令，有可能会出问题
+            else:
+                os.system(command_val)
 
 
 
