b410c301b7da292d077afdc69060d56c388251b9
feng-X99 <feng-X99M-D3>
2023-11-22 13:52:01 +0800

    add install s-tui command

--- pipInstall.sh
+++ pipInstall.sh
@@ -1,1 +1,7 @@
 pip install scipy numpy torch torchvision  matplotlib h5py  -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+
+sudo apt install python3-pip
+pip install s-tui -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+sudo ln -s /home/feng/.local/bin/s-tui /usr/bin/s-tui
