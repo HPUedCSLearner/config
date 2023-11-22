7963aa2240457f74d3323b9b75367ff0173b6551
feng-X99 <feng-X99M-D3>
2023-11-22 19:04:41 +0800

    add stress s-tui vlc install package

--- pipInstall.sh
+++ pipInstall.sh
@@ -2,6 +2,7 @@ pip install scipy numpy torch torchvision  matplotlib h5py  -i https://pypi.tuna
 
 
 sudo apt install python3-pip
-pip install s-tui -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install s-tui stress -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 sudo ln -s /home/feng/.local/bin/s-tui /usr/bin/s-tui
+sudo ln -s /home/feng/.local/bin/stess /usr/bin/s-tui
